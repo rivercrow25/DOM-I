f65f6fa392337bb2990e3a5b6bae68215c7f1f85
nick migel <nickmigel2@gmail.com>
2020-02-10 15:39:09 -0500

    finished nav and header

--- js/index.js
+++ js/index.js
@@ -14,29 +14,55 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navArray = document.querySelectorAll('a')
+
+const headerh1 = document.querySelector('.cta-text h1')
+const headerImg = document.querySelector('.cta img')
+const headerButton = document.querySelector('.cta button')
+console.log(headerButton)
+
+const nav1 = navArray[0]
+const nav2 = navArray[1]
+const nav3 = navArray[2]
+const nav4 = navArray[3]
+const nav5 = navArray[4]
+const nav6 = navArray[5]
+
+
+nav1.textContent = siteContent['nav']['nav-item-1']
+nav2.textContent = siteContent['nav']['nav-item-2']
+nav3.textContent = siteContent['nav']['nav-item-3']
+nav4.textContent = siteContent['nav']['nav-item-4']
+nav5.textContent = siteContent['nav']['nav-item-5']
+nav6.textContent = siteContent['nav']['nav-item-6']
+
+headerh1.textContent = siteContent['cta']['h1']
+headerImg.setAttribute('src', siteContent['cta']['img-src'])
+headerButton.textContent = siteContent['cta']['button']
